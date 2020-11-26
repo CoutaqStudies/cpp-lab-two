c4876d5308be160efca010a60d3b9e962882271f
Michael Melikov <coutaq@gmail.com>
2020-11-26 18:34:52 +0300

    removed useless junk

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/coutaq/Documents/VSCode/lab-two-cpp
-# Build directory: /Users/coutaq/Documents/VSCode/lab-two-cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
